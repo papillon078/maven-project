787034f267dcbdc41298161a69147a303f65d4ba
LudovicMorin <55787172+papillon078@users.noreply.github.com>
2021-05-31 16:33:52 +0200

    fix spaces

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,21 +1,21 @@
 package com.example;
 
 /**
-*classe greeter qui dit bonjour a qq
+* classe greeter qui dit bonjour a qq
 */
 public class Greeter {
 
   /**
-  *constructeur de classe
+  * constructeur de classe
   */
   public Greeter() {
 
   }
   
   /**
-  *methode qui dite bonjour a quelqu'un
-  *@param String
-  *@return String
+  * methode qui dite bonjour a quelqu'un
+  * @param someone personne a qui on dit bonjour
+  * @return bonjour en String
   */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
