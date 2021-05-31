44d47e65114a7f5b3bddc8bc5880def55bd2fd49
LudovicMorin <55787172+papillon078@users.noreply.github.com>
2021-05-31 16:22:04 +0200

    annotation fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -4,13 +4,16 @@ package com.example;
 public class Greeter {
 
   /**
-  *methode qui dite bonjour a quelqu'un
-  *@return String
+  *classe qui dite bonjour a quelqu'un
+  *
   */
   public Greeter() {
 
   }
-
+  /**
+  *methode qui dite bonjour a quelqu'un
+  *@return String
+  */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
